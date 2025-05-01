26c319ad1011e86bb4bd372f28293b42096dedfb
Leandro Poletti de Oliveira <leandropoletti2004@gmail.com>
2025-05-01 11:08:46 -0300

    rebase

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // TRY THIS: Try running your application with "flutter run". You'll see
+          // the application has a purple toolbar. Then, without quitting the app,
+          // try changing the seedColor in the colorScheme below to Colors.green
+          // and then invoke "hot reload" (save your changes or press the "hot
+          // reload" button in a Flutter-supported IDE, or press "r" if you used
+          // the command line to start the app).
+          //
+          // Notice that the counter didn't reset back to zero; the application
+          // state is not lost during the reload. To reset the state, use hot
+          // restart instead.
+          //
+          // This works for code too, not just values: Most code changes can be
+          // tested with just a hot reload.
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: ListaTodo());
+  }
+}
+
+class ListaTodo extends StatelessWidget {
+  const ListaTodo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (b) {
+                return SizedBox.expand(
+                  child: Container(
+                    decoration:
+                        BoxDecoration(borderRadius: BorderRadius.circular(12)),
+                    padding: EdgeInsets.only(top: 50),
+                    child: Column(
+
+                      children: [Text("data")],
+                    ),
+                  ),
+                );
+              });
+        },
+        backgroundColor: Colors.blue,
+        elevation: 12,
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      appBar: AppBar(
+        title: Text("Projeto Quirino todo"),
+        backgroundColor: Colors.blue,
+        titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+      ),
+    );
+  }
+}
